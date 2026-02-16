01d942deae421a023c63590d3d48e307dc00ef4b
Francisco069 <jvasillo300@gmail.com>
2026-02-16 11:54:46 -0600

    Creacion del proyecto

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+const name = 'Javier';
+const age = 21;
+const profession = 'Computer Systems Engineer';
+
+</script>
+
+<template>
+  <div>
+  <h1 class="bg-amber-50 text-center">Welcome to my portfolio, I'm {{ name }}!</h1>
+  <h2>Age: {{ age }}</h2>
+  <h2>Profession: {{ profession }}</h2>
+  </div>
+</template> 
